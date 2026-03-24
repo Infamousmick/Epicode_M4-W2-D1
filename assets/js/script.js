98a0b31f893d33de75ab1dbcaa9a605884ef0cc5
Infamousmick <mmarchese6690@gmail.com>
2026-03-24 21:06:30 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,20 @@ const getData = async (url) => {
 };
 
 const search = () => {
-  const searchInput = document.querySelector("#search").value;
-  getData(`${URL}${searchInput}`).then((res) => cyclePhotos(res));
+  const searchInput = document.querySelector("#search").value.trim();
+  if (!searchInput) {
+    console.warn("Ricerca vuota");
+    return;
+  }
+  const rowContainer = document.querySelector("main .container .row ");
+  getData(`${URL}${searchInput}`).then((res) => {
+    clearScreen(rowContainer);
+    const outputArr = mapData(res);
+    const likeRadio = radio();
+    const filteredArr = filterArr(likeRadio, outputArr);
+
+    filteredArr.forEach((photo) => createCards(photo, rowContainer));
+  });
 };
 
 const searchInput = document
@@ -28,34 +40,70 @@ const searchInput = document
     }
   });
 
-const cyclePhotos = (data) => {
-  const rowContainer = document.querySelector("main .container .row ");
-  rowContainer.innerHTML = "";
-  data.photos.forEach((photo) => {
-    createCards(photo.alt, photo.src.small, photo.photographer, rowContainer);
-  });
+const clearScreen = (container) => {
+  container.innerHTML = "";
 };
 
-const createCards = (alt, imgUrl, author, container) => {
+const mapData = (data) => {
+  const arr = data.photos.map((photo) => {
+    return {
+      alt: photo.alt,
+      img: photo.src.small,
+      author: photo.photographer,
+      isLiked: photo.liked,
+    };
+  });
+
+  return arr;
+};
+
+const radio = () => {
+  const checkedRadio = document.querySelector(
+    `input[name="fan-speed"]:checked`,
+  );
+
+  if (checkedRadio) {
+    return checkedRadio.value;
+  }
+};
+
+const filterArr = (liked, arr) => {
+  return arr.filter((photo) => {
+    if (liked === "liked") {
+      return photo.isLiked === true;
+    } else {
+      return photo.isLiked === false;
+    }
+  });
+};
+const createCards = (data, container) => {
+  const col = document.createElement("div");
+  col.setAttribute("class", "col");
+
   const card = document.createElement("div");
-  card.setAttribute("class", "card col");
+  card.setAttribute("class", "card h-100");
 
   const img = document.createElement("img");
-  img.src = imgUrl;
+  img.src = data.img;
   img.setAttribute("class", "card-img-top");
 
   const cardBody = document.createElement("div");
   cardBody.setAttribute("class", "card-body");
 
   const h5 = document.createElement("h5");
-  h5.textContent = alt;
+  h5.textContent = data.alt;
   h5.setAttribute("class", "card-title");
 
-  const p = document.createElement("p");
-  p.textContent = author;
-  p.setAttribute("class", "card-text");
+  const cardFooter = document.createElement("div");
+  cardFooter.setAttribute("class", "card-footer d-flex justify-content-center");
 
-  cardBody.append(h5, p);
-  card.append(img, cardBody);
-  container.appendChild(card);
+  const small = document.createElement("small");
+  small.textContent = data.author;
+  small.setAttribute("class", "text-muted");
+
+  cardFooter.append(small);
+  cardBody.append(h5);
+  card.append(img, cardBody, cardFooter);
+  col.appendChild(card);
+  container.appendChild(col);
 };
